ad8d6ac07fbcfd5e345c2f5dc8528536f9f92be1
sedeeki <morbhaya@gmail.com>
2024-07-08 15:13:11 +0500

    Learned property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,16 +14,11 @@ export class UserComponent {
 
 @Component({
   selector: 'app-root',
-  template: `
-    @for (user of users; track user.id) {
-      <p>{{ user.name }}</p>
-    }
-  `,
+  template: `<div [contentEditable]="isEditable"></div>`,
   standalone: true,
   imports: [],
 })
 export class AppComponent {
-  operatingSystems = [{id: 'win', name: 'Windows'}, {id: 'osx', name: 'MacOS'}, {id: 'linux', name: 'Linux'}]; 
-  users = [{id: 0, name: 'Sarah'}, {id: 1, name: 'Amy'}, {id: 2, name: 'Rachel'}, {id: 3, name: 'Jessica'}, {id: 4, name: 'Poornima'}];
+  isEditable = true;
 }
 
