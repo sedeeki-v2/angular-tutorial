7924290c35463ccdb3ac3cfe32c8465caffda778
sedeeki <morbhaya@gmail.com>
2024-07-08 15:07:41 +0500

    Learned For Loops

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,17 +15,15 @@ export class UserComponent {
 @Component({
   selector: 'app-root',
   template: `
-    @if (isLoggedIn) {
-      <p>You are logged in!</p>
-    } @else {
-      <p>You are not logged in!</p>
+    @for (user of users; track user.id) {
+      <p>{{ user.name }}</p>
     }
-  
   `,
   standalone: true,
   imports: [],
 })
 export class AppComponent {
-  isLoggedIn = false;
+  operatingSystems = [{id: 'win', name: 'Windows'}, {id: 'osx', name: 'MacOS'}, {id: 'linux', name: 'Linux'}]; 
+  users = [{id: 0, name: 'Sarah'}, {id: 1, name: 'Amy'}, {id: 2, name: 'Rachel'}, {id: 3, name: 'Jessica'}, {id: 4, name: 'Poornima'}];
 }
 
