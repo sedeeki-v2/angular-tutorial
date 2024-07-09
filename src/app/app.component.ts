9ae159413fc33a64614a2a2ec1e30616b8250d26
sedeeki <morbhaya@gmail.com>
2024-07-09 11:38:54 +0500

    Learned how to create a pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,14 @@
 import {Component} from '@angular/core';
-import {DecimalPipe, DatePipe, CurrencyPipe} from '@angular/common';
+import {ReversePipe} from './reverse.pipe';
 
 @Component({
   selector: 'app-root',
   template: `
-    <ul>
-      <li>Number with "decimal" {{ num | number:'3.2-2' }}</li>
-      <li>Date with "date" {{ birthday | date:'medium' }}</li>
-      <li>Currency with "currency" {{ cost | currency }}</li>
-    </ul>
+    Reverse Machine: {{ word | reverse }}
   `,
   standalone: true,
-  imports: [DecimalPipe, DatePipe, CurrencyPipe],
+  imports: [ReversePipe],
 })
 export class AppComponent {
-  num = 103.1234;
-  birthday = new Date(2023, 3, 2);
-  cost = 4560.34;
+  word = 'You are a champion';
 }
