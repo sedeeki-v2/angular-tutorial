4eb898d6bfd59284e644783f5137cc064ec5a5d1
sedeeki <morbhaya@gmail.com>
2024-07-09 09:42:41 +0500

    Learned about images

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -1,12 +1,29 @@
-import {Component, Input} from '@angular/core';
+import {Component} from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
+  standalone: true,
   selector: 'app-user',
   template: `
-    <p>The user's name is {{occupation}}</p>
+    <p>Username: {{ username }}</p>
+    <p>Preferred Framework:</p>
+    <ul>
+      <li>
+        Static Image:
+        <div class="image-container">
+          <img ngSrc="/assets/favicon.ico" height="60" width="80" priority alt="Angular logo" />
+        </div>
+      </li>
+      <li>
+        Dynamic Image:
+        <img [ngSrc]="logoUrl" height="60" width="80" [alt]="logoAlt" />
+      </li>
+    </ul>
   `,
-  standalone: true,
+  imports: [NgOptimizedImage],
 })
 export class UserComponent {
-    @Input() occupation = '';
+  logoUrl = '/assets/favicon.ico';
+  logoAlt = 'Angular logo';
+  username = 'youngTech';
 }
