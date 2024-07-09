7f26eaaccea9aca64667e15cf3581c51b63c1741
sedeeki <morbhaya@gmail.com>
2024-07-09 11:26:31 +0500

    Learned about dependency injection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,15 @@
-import {Component} from '@angular/core';
-import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
+import {Component, inject} from '@angular/core';
+import {CarService} from './car.service';
+
 @Component({
   selector: 'app-root',
+  template: `<p>Car Listing: {{ display }}</p>`,
   standalone: true,
-  template: `
-    <form 
-      [formGroup]="profileForm" 
-      (ngSubmit)="handleSubmit()"
-    >
-      <label>Name
-        <input type="text" formControlName="name"/>
-      </label>
-      <label>Email
-        <input type="email" formControlName="email"/>
-      </label>
-      <button type="submit" [disabled]="!profileForm.valid">Submit</button>
-    </form>
-
-    <h2>Profile Form</h2>
-      <p>Name: {{ profileForm.value.name }}</p>
-      <p>Email: {{ profileForm.value.email }}</p>
-  `,
-  imports: [ReactiveFormsModule],
 })
-
 export class AppComponent {
-  profileForm = new FormGroup({
-    name: new FormControl('', Validators.required),
-    email: new FormControl('', [Validators.required, Validators.email]),
-  });
+  display = '';
 
-  handleSubmit() {
-    alert(
-      this.profileForm.value.name + "|" + this.profileForm.value.email
-    )
+  constructor(private carService: CarService) {
+    this.display = this.carService.getCars().join(' ⭐️ ');
   }
 }
